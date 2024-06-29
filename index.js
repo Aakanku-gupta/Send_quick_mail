25017197fc1efbd2d52c774674fd8487ae2de47a
Aakanksha Gupta <119932567+Aakanku-gupta@users.noreply.github.com>
2024-06-29 12:26:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    // hide preloader when content is loaded
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+};
